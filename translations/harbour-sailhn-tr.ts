194a73fea7d0f00d56ed4776681937770545ed6a
Andrea Scarpino <me@andreascarpino.it>
2015-07-23 18:18:01 +0200

    Add Turkish translation

---
+++ translations/harbour-sailhn-tr.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="tr" version="2.1">
+<context>
+    <name>AskStoriesPage</name>
+    <message>
+        <location filename="../qml/pages/AskStoriesPage.qml" line="41"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AskStoriesPage.qml" line="49"/>
+        <source>Load more</source>
+        <translation>Daha fazla yükle</translation>
+    </message>
+</context>
+<context>
+    <name>CommentsPage</name>
+    <message>
+        <location filename="../qml/pages/CommentsPage.qml" line="50"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CommentsPage.qml" line="63"/>
+        <source>Load more</source>
+        <translation>Daha fazla yükle</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CommentsPage.qml" line="79"/>
+        <source>Comments</source>
+        <translation>Yorum</translation>
+    </message>
+</context>
+<context>
+    <name>ItemDelegate</name>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="53"/>
+        <source>point</source>
+        <translation>nokta</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="55"/>
+        <source>points</source>
+        <translation>noktalar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="61"/>
+        <source>comment</source>
+        <translation>yorum</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="63"/>
+        <source>comments</source>
+        <translation>yorum</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="87"/>
+        <source>Open external url</source>
+        <translation>Dış akış aç</translation>
+    </message>
+</context>
+<context>
+    <name>JobStoriesPage</name>
+    <message>
+        <location filename="../qml/pages/JobStoriesPage.qml" line="41"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/JobStoriesPage.qml" line="49"/>
+        <source>Load more</source>
+        <translation>Daha fazla yükle</translation>
+    </message>
+</context>
+<context>
+    <name>NewStoriesPage</name>
+    <message>
+        <location filename="../qml/pages/NewStoriesPage.qml" line="41"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NewStoriesPage.qml" line="49"/>
+        <source>Load more</source>
+        <translation>Daha fazla yükle</translation>
+    </message>
+</context>
+<context>
+    <name>RepliesPage</name>
+    <message>
+        <location filename="../qml/pages/RepliesPage.qml" line="45"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RepliesPage.qml" line="58"/>
+        <source>Load more</source>
+        <translation>Daha fazla yükle</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RepliesPage.qml" line="74"/>
+        <source>Replies</source>
+        <translation>Yanıtlar</translation>
+    </message>
+</context>
+<context>
+    <name>ReplyDelegate</name>
+    <message>
+        <location filename="../qml/pages/ReplyDelegate.qml" line="60"/>
+        <source>reply</source>
+        <translation>yanıt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReplyDelegate.qml" line="62"/>
+        <source>replies</source>
+        <translation>yanıtlar</translation>
+    </message>
+</context>
+<context>
+    <name>ShowStoriesPage</name>
+    <message>
+        <location filename="../qml/pages/ShowStoriesPage.qml" line="41"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ShowStoriesPage.qml" line="49"/>
+        <source>Load more</source>
+        <translation>Daha fazla yükle</translation>
+    </message>
+</context>
+<context>
+    <name>TopStoriesPage</name>
+    <message>
+        <location filename="../qml/pages/TopStoriesPage.qml" line="41"/>
+        <source>Refresh</source>
+        <translation>Yenile</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/TopStoriesPage.qml" line="49"/>
+        <source>Load more</source>
+        <translation>Daha fazla yükle</translation>
+    </message>
+</context>
+</TS>
