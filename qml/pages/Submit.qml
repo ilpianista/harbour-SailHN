ilpianista/harbour-SailHN

commit bda87361f536d35b1879579cdc6d03482601984c

Do not enable the submit button if there's no text

qml/pages/Submit.qml
@@ -65,12 +65,16 @@ Page {
                 width: parent.width
                 focus: true
                 placeholderText: qsTr("Title")
+
+                onTextChanged: submit.enabled = (text.length > 0 && (url.text.length > 0 || text.text.length > 0))
             }
 
             TextField {
                 id: url
                 width: parent.width
                 placeholderText: qsTr("Url")
+
+                onTextChanged: submit.enabled = (title.text.length > 0 && (text.length > 0 || text.text.length > 0))
             }
 
             Label {
@@ -83,20 +87,21 @@ Page {
                 id: text
                 width: parent.width
                 placeholderText: qsTr("Text")
+
+                onTextChanged: submit.enabled = (title.text.length > 0 && (url.text.length > 0 || text.length > 0))
             }
 
             Button {
                 id: submit
                 text: qsTr("Submit");
                 anchors.horizontalCenter: parent.horizontalCenter
+                enabled: false
 
                 onClicked: {
-                    if (title.text.length > 0 && (url.text.length > 0 || text.text.length > 0)) {
-                        manager.submit(title.text, url.text, text.text);
-                        submit.enabled = false;
-                        busy.visible = busy.running = true;
-                        msg.visible = false;
-                    }
+                    manager.submit(title.text, url.text, text.text);
+                    submit.enabled = false;
+                    busy.visible = busy.running = true;
+                    msg.visible = false;
                 }
             }
 

commit 0030ca3af3e793ec8d7cb6dfdceacda582aeb1c5

Fix regression caused by bda87361

qml/pages/Submit.qml
@@ -66,7 +66,7 @@ Page {
                 focus: true
                 placeholderText: qsTr("Title")
 
-                onTextChanged: submit.enabled = (text.length > 0 && (url.text.length > 0 || text.text.length > 0))
+                onTextChanged: submit.enabled = (text.length > 0 && (url.text.length > 0 || itemText.text.length > 0))
             }
 
             TextField {
@@ -74,7 +74,7 @@ Page {
                 width: parent.width
                 placeholderText: qsTr("Url")
 
-                onTextChanged: submit.enabled = (title.text.length > 0 && (text.length > 0 || text.text.length > 0))
+                onTextChanged: submit.enabled = (title.text.length > 0 && (text.length > 0 || itemText.text.length > 0))
             }
 
             Label {
@@ -84,7 +84,7 @@ Page {
             }
 
             TextArea {
-                id: text
+                id: itemText
                 width: parent.width
                 placeholderText: qsTr("Text")
 
@@ -98,7 +98,7 @@ Page {
                 enabled: false
 
                 onClicked: {
-                    manager.submit(title.text, url.text, text.text);
+                    manager.submit(title.text, url.text, itemText.text);
                     submit.enabled = false;
                     busy.visible = busy.running = true;
                     msg.visible = false;