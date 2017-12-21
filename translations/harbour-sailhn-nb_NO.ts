1d0b6905e13227206bc20db2acd4b4aa2460039f
Allan Nordhøy <epost@anotheragency.no>
2017-12-21 09:51:32 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ translations/harbour-sailhn-nb_NO.ts
@@ -0,0 +1,240 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>CommentsPage</name>
+    <message>
+        <location filename="../qml/pages/CommentsPage.qml" line="51"/>
+        <source>Copy external url</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CommentsPage.qml" line="60"/>
+        <source>Reply</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CommentsPage.qml" line="67"/>
+        <source>Refresh</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CommentsPage.qml" line="80"/>
+        <source>Load more</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CommentsPage.qml" line="96"/>
+        <source>Comments</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ItemDelegate</name>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="52"/>
+        <source>point</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="54"/>
+        <source>points</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="60"/>
+        <source>comment</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ItemDelegate.qml" line="62"/>
+        <source>comments</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>RepliesPage</name>
+    <message>
+        <location filename="../qml/pages/RepliesPage.qml" line="48"/>
+        <source>Reply</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RepliesPage.qml" line="55"/>
+        <source>Refresh</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RepliesPage.qml" line="68"/>
+        <source>Load more</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RepliesPage.qml" line="84"/>
+        <source>Replies</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Reply</name>
+    <message>
+        <location filename="../qml/pages/Reply.qml" line="44"/>
+        <source>Error when sending</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Reply.qml" line="46"/>
+        <source>Commented!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Reply.qml" line="61"/>
+        <source>Reply</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Reply.qml" line="68"/>
+        <source>Text</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Reply.qml" line="73"/>
+        <source>Add Comment</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ReplyDelegate</name>
+    <message>
+        <location filename="../qml/pages/ReplyDelegate.qml" line="59"/>
+        <source>reply</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ReplyDelegate.qml" line="61"/>
+        <source>replies</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="61"/>
+        <source>Logout</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="113"/>
+        <source>Login failed</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="76"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="82"/>
+        <source>Username</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="90"/>
+        <source>Password</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="98"/>
+        <location filename="../qml/pages/Settings.qml" line="186"/>
+        <source>Login</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="130"/>
+        <source>User details</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="136"/>
+        <source>created</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="142"/>
+        <source>karma</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="148"/>
+        <source>About</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="184"/>
+        <source>Logged</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>StoriesListView</name>
+    <message>
+        <location filename="../qml/pages/StoriesListView.qml" line="49"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoriesListView.qml" line="56"/>
+        <source>Submit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoriesListView.qml" line="63"/>
+        <source>Refresh</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoriesListView.qml" line="72"/>
+        <source>Load more</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Submit</name>
+    <message>
+        <location filename="../qml/pages/Submit.qml" line="45"/>
+        <source>Submitted!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Submit.qml" line="60"/>
+        <location filename="../qml/pages/Submit.qml" line="96"/>
+        <source>Submit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Submit.qml" line="67"/>
+        <source>Title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Submit.qml" line="75"/>
+        <source>Url</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Submit.qml" line="81"/>
+        <source>or</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Submit.qml" line="89"/>
+        <source>Text</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Submit.qml" line="43"/>
+        <source>Error during submission</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
